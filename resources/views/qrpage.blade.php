cfa8c05436286195980db0cc2595eacdad660e0f
EchtTech <131341398+GauravS1701@users.noreply.github.com>
2023-07-20 14:53:55 +0530

    change the url of qr code in blade file add all the qrs

--- resources/views/qrpage.blade.php
+++ resources/views/qrpage.blade.php
@@ -4,8 +4,20 @@
     <title>Echttech QR</title>
 </head>
 <body>
+    <p>Akansha Chauhan</p>    
 <div class="visible-print text-center">
     {!! QrCode::size(120)->generate('https://qr.echttech.com/public/getdetails/1'); !!}
 </div>
+    
+    <p>Praveen Kumar Srivastava</p>    
+<div class="visible-print text-center">
+    {!! QrCode::size(120)->generate('https://qr.echttech.com/public/getdetails/2'); !!}
+</div><!-- comment -->
+
+<p>Neeraj Kumar</p>
+<div class="visible-print text-center">
+    {!! QrCode::size(120)->generate('https://qr.echttech.com/public/getdetails/3'); !!}
+</div>
+    
 </body>
 </html>
