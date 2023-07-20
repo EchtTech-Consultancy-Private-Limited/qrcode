2d17ed0dc19754f1473c86f0b1b317a434910b23
EchtTech <131341398+GauravS1701@users.noreply.github.com>
2023-07-20 14:58:50 +0530

    change the url of qr code in blade file

--- resources/views/qrpage.blade.php
+++ resources/views/qrpage.blade.php
@@ -6,17 +6,17 @@
 <body>
     <p>Akansha Chauhan</p>    
 <div class="visible-print text-center">
-    {!! QrCode::size(80)->generate('https://qr.echttech.com/public/getdetails/1'); !!}
+    {!! QrCode::size(50)->generate('https://qr.echttech.com/public/getdetails/1'); !!}
 </div>
     
     <p>Praveen Kumar Srivastava</p>    
 <div class="visible-print text-center">
-    {!! QrCode::size(80)->generate('https://qr.echttech.com/public/getdetails/2'); !!}
+    {!! QrCode::size(50)->generate('https://qr.echttech.com/public/getdetails/2'); !!}
 </div><!-- comment -->
 
 <p>Neeraj Kumar</p>
 <div class="visible-print text-center">
-    {!! QrCode::size(80)->generate('https://qr.echttech.com/public/getdetails/3'); !!}
+    {!! QrCode::size(50)->generate('https://qr.echttech.com/public/getdetails/3'); !!}
 </div>
     
 </body>
